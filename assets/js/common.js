7482df91c65c573925d00da445c11f0125ebc28b
xyang <xyang4le@gmail.com>
2024-09-20 12:15:36 +0800

    homepage

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,3 +11,32 @@ $(function () {
     })
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var container = document.getElementById('toggleContainer');
+    if (container.classList.contains('hidden')) {
+        container.classList.remove('hidden');
+        container.classList.add('visible');
+    } else {
+        container.classList.remove('visible');
+        container.classList.add('hidden');
+    }
+});
+
+// Function to toggle BibTeX container
+function toggleBibtex() {
+    var bibtexContainer = document.querySelector('.bibtex-container');
+    if (bibtexContainer.classList.contains('hidden')) {
+        bibtexContainer.classList.remove('hidden');
+        bibtexContainer.classList.add('visible');
+    } else {
+        bibtexContainer.classList.remove('visible');
+        bibtexContainer.classList.add('hidden');
+    }
+}
+
+// Example of adding a click event to a link
+document.querySelector('a[href="#bibtex"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    toggleBibtex();
+});
